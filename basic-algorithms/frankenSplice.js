db62fe1e682f484d17c96ef578f838b67caa3c5c
onTheDL <daniel.laserna@protonmail.com>
2020-06-16 01:12:02 -0400

    Add bouncer.js, frankenSplice.js, and getIndextoIns.js

---
+++ basic-algorithms/frankenSplice.js
@@ -0,0 +1,18 @@
+/* 
+INSTRUCTIONS
+You are given two arrays and an index.
+
+Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  let result = arr2.slice();
+  result.splice(n, 0, ...arr1);
+  return result;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
